0bdd7a5bc99aa72d3e382f2ea00f361a6d95e9ba
Manish Sahu <manissh7352@gmail.com>
2021-08-17 19:28:08 +0530

    made all the changes

--- mail/contact.php
+++ mail/contact.php
@@ -9,11 +9,11 @@ $email = strip_tags(htmlspecialchars($_POST['email']));
 $m_subject = strip_tags(htmlspecialchars($_POST['subject']));
 $message = strip_tags(htmlspecialchars($_POST['message']));
 
-$to = "info@example.com"; // Change this email to your //
+$to = "exports@ramarutfibc.com"; // Change this email to your //
 $subject = "$m_subject:  $name";
 $body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\n\nEmail: $email\n\nSubject: $m_subject\n\nMessage: $message";
 $header = "From: $email";
-$header .= "Reply-To: $email";	
+//$header .= "Reply-To: $email";	
 
 if(!mail($to, $subject, $body, $header))
   http_response_code(500);
